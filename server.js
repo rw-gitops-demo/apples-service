c303994e96489d165c34a28ce328c55fa5a18156
Richard Walters <richard.walters@liberis.com>
2024-01-08 16:13:45 +0000

    feat: state apples are yummy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send(`Apples are ${process.env.COLOUR || 'blue'}.`)
+  res.send(`Apples are ${process.env.COLOUR || 'blue and yummy'}.`)
 })
 
 app.listen(port, () => {
